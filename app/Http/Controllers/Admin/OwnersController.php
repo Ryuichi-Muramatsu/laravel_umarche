2fa3165cac383f53d704c8709bcf478d8f23f5f8
Ryuichi-Muramatsu <ryuichi.work23@gmail.com>
2023-02-13 18:09:24 +0900

    Store

--- app/Http/Controllers/Admin/OwnersController.php
+++ app/Http/Controllers/Admin/OwnersController.php
@@ -8,6 +8,9 @@ use App\Models\Owner; //Eloquent エロクアント
 use Illuminate\Support\Facades\DB; //QueryBuilder
 use Carbon\Carbon;
 use PhpParser\Node\Expr\Cast;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Validation\Rules;
+
 
 class OwnersController extends Controller
 {
@@ -36,9 +39,9 @@ class OwnersController extends Controller
 
         // dd($e_all,$q_get,$q_first,$c_test);
 
-        $owners = Owner::select('name','email','created_at')->get();
+        $owners = Owner::select('name', 'email', 'created_at')->get();
 
-        return view('admin.owners.index',compact('owners'));
+        return view('admin.owners.index', compact('owners'));
     }
 
     /**
@@ -59,7 +62,22 @@ class OwnersController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        // $request->name;
+        $request->validate([
+            'name' => ['required', 'string', 'max:255'],
+            'email' => ['required', 'string', 'email', 'max:255', 'unique:owners'],
+            'password' => ['required','confirmed', Rules\Password::defaults()],
+        ]);
+
+        Owner::create([
+            'name' => $request->name,
+            'email' => $request->email,
+            'password' => Hash::make($request->password),
+        ]);
+
+        return redirect()
+        ->route('admin.owners.index')
+        ->with('message','オーナー登録を実施しました。');
     }
 
     /**
