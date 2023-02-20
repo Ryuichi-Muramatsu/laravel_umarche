e1b8563d5f019abb70b126438345cc6a91f85eb8
Ryuichi-Muramatsu <ryuichi.work23@gmail.com>
2023-02-20 18:29:52 +0900

    product create

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -20,8 +20,7 @@ class DatabaseSeeder extends Seeder
             ShopSeeder::class,
             ImageSeeder::class,
             CategorySeeder::class,
-            ProductSeeder::class,
-            StockSeeder::class,
+            //  
         ]);
     }
 }
