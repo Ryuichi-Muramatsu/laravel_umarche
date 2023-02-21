65b4b59f947cf219dc6031bfcc49def35b1b85ed
Ryuichi-Muramatsu <ryuichi.work23@gmail.com>
2023-02-21 12:09:33 +0900

    product store

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -13,19 +13,37 @@ class Product extends Model
 {
     use HasFactory;
 
-    public function shop(){
+    protected $fillable = [
+        'shop_id',
+        'name',
+        'infomation',
+        'price',
+        'is_selling',
+        'sort_order',
+        'secondary_category_id',
+        'image1',
+        'image2',
+        'image3',
+        'image4',
+    ];
+
+    public function shop()
+    {
         return $this->belongsTo(Shop::class);
     }
 
-    public function category(){
-        return $this->belongsTo(SecondaryCategory::class,'secondary_category_id');
+    public function category()
+    {
+        return $this->belongsTo(SecondaryCategory::class, 'secondary_category_id');
     }
 
-    public function imageFirst(){
-        return $this->belongsTo(Image::class,'image1','id');
+    public function imageFirst()
+    {
+        return $this->belongsTo(Image::class, 'image1', 'id');
     }
 
-    public function stock(){
+    public function stock()
+    {
         return $this->hasMany(Stock::class);
     }
 }
