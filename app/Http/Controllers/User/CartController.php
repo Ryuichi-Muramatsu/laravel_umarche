80e26af90dc3f8d95c586f39f39e1a289f9a409e
Ryuichi-Muramatsu <ryuichi.work23@gmail.com>
2023-02-28 14:22:32 +0900

    stripeのcontroller側の設定

--- app/Http/Controllers/User/CartController.php
+++ app/Http/Controllers/User/CartController.php
@@ -16,21 +16,22 @@ class CartController extends Controller
         $products = $user->products;
         $totalPrice = 0;
 
-        foreach($products as $product){
+        foreach ($products as $product) {
             $totalPrice += $product->price * $product->pivot->quantity;
         }
 
-        return view('user.cart',compact('products','totalPrice'));
+        return view('user.cart', compact('products', 'totalPrice'));
     }
-    public function add(Request $request){
-        
-        $itemInCart = Cart::where('product_id',$request->product_id)
-        ->where('user_id',Auth::id())->first();
+    public function add(Request $request)
+    {
+
+        $itemInCart = Cart::where('product_id', $request->product_id)
+            ->where('user_id', Auth::id())->first();
 
         if ($itemInCart) {
             $itemInCart->quantity += $request->quantity;
             $itemInCart->save();
-        }else{
+        } else {
             Cart::create([
                 'user_id' => Auth::id(),
                 'product_id' => $request->product_id,
@@ -38,5 +39,49 @@ class CartController extends Controller
             ]);
         }
         return redirect()->route('user.cart.index');
+    }
+
+    public function delete($id)
+    {
+        Cart::where('product_id', $id)
+            ->where('user_id', Auth::id())
+            ->delete();
+
+        return redirect()->route('user.cart.index');
+    }
+
+    public function checkout()
+    {
+
+        $user = User::findOrFail(Auth::id());
+        $products = $user->products;
+
+        $lineItems = [];
+
+        foreach ($products as $product) {
+            $lineItem = [
+                'name' => $product->name,
+                'description' => $product->infomation,
+                'amount' => $product->price,
+                'currency' => 'jpy',
+                'quantity' => $product->pivot->quantity,
+            ];
+
+            array_push($lineItems, $lineItem);
+        }
+        // dd($lineItems);
+
+        \Stripe\Stripe::setApiKey(env('STRIPE_SECRET_KEY'));
+
+        $session = \Stripe\Checkout\Session::create([
+            'lineItems' => [$lineItems],
+            'mode' => 'paymemt',
+            'success_url' => route('user.items.index'),
+            'cancel_url' => route('user.cart.index'),
+        ]);
+
+        $publicKey = env('STRIPE_PUBLIC_KEY');
+
+        return view('user.checkout',compact('session','publicKey'));
     }
 }
