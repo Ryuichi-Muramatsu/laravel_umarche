e0e58c572f8f80ca4fe4ba597705d7997f1b30bd
Ryuichi-Muramatsu <ryuichi.work23@gmail.com>
2023-03-02 14:32:55 +0900

    オーナー向けメール送信

--- app/Http/Controllers/User/CartController.php
+++ app/Http/Controllers/User/CartController.php
@@ -10,7 +10,8 @@ use App\Models\Stock;
 use Illuminate\Support\Facades\Auth;
 use App\Services\CartService;
 use App\Jobs\SendThanksMail;
-
+use App\Jobs\SendOrderdMail;
+use App\Jobs\SendOrderedMail;
 
 class CartController extends Controller
 {
@@ -26,6 +27,7 @@ class CartController extends Controller
 
         return view('user.cart', compact('products', 'totalPrice'));
     }
+
     public function add(Request $request)
     {
 
@@ -57,12 +59,7 @@ class CartController extends Controller
     public function checkout()
     {
 
-        $items = Cart::where('user_id', Auth::id())->get();
-        $products = CartService::getItemsInCart($items);
-        $user = User::findOrFail(Auth::id());
-
-        SendThanksMail::dispatch($products,$user);
-        dd('ユーザーメール送信テスト');
+        
 
         $user = User::findOrFail(Auth::id());
         $products = $user->products;
@@ -119,6 +116,16 @@ class CartController extends Controller
 
     public function success()
     {
+
+        $items = Cart::where('user_id', Auth::id())->get();
+        $products = CartService::getItemsInCart($items);
+        $user = User::findOrFail(Auth::id());
+
+        SendThanksMail::dispatch($products,$user);
+        foreach ($products as $product) {
+            SendOrderedMail::dispatch($product,$user);
+        }
+
         Cart::where('user_id', Auth::id())->delete();
 
         return redirect()->route('user.items.index');
